9a8d8ad6c65ce727e198e5a5c3aa5e7ccdddec5b
Nuno Martins <nunojorge90@hotmail.com>
2018-04-30 02:45:04 +0100

    template

---
+++ proj/src/components/BackgroundCard.vue
@@ -0,0 +1,42 @@
+<template>
+   <button class="card" @click="click()">
+       {{title}}
+   </button>
+</template>
+
+<script>
+export default {
+  name: "BackgroundCard",
+  props: ['title'],
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+      click (){
+          this.$emit('click')
+      }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  text-align: center;
+  margin: auto;
+  background-color:rgb(255, 255, 255);
+  width: 20%;
+  height: 60px;
+  padding:10px;
+  border:solid;
+  border-color: green;
+  border-width: 2px;
+  border-radius: 30px;
+  font-size:25px;
+}
+.card:hover {
+    background-color:rgb(201, 201, 201);
+}
+</style>
--- proj/src/components/Main.vue
+++ proj/src/components/Main.vue
@@ -1,78 +1,64 @@
 <template>
-   <div class="main">
-      <img src="../assets/logo.png">
-      <h1>{{ msg }}</h1>
-      <h2>Essential Links</h2>
+  <div class="main">
+    <!-- Here goes the background choosing menu -->
+    <div class="backgrounds">
+      <h2>Choose the Background</h2>
       <ul>
-         <li>
-            <a href="https://vuejs.org" target="_blank">
-            Core Docs
-            </a>
-         </li>
-         <li>
-            <a href="https://forum.vuejs.org" target="_blank">
-            Forum
-            </a>
-         </li>
-         <li>
-            <a href="https://chat.vuejs.org" target="_blank">
-            Community Chat
-            </a>
-         </li>
-         <li>
-            <a href="https://twitter.com/vuejs" target="_blank">
-            Twitter
-            </a>
-         </li>
-         <br>
-         <li>
-            <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
-            Docs for This Template
-            </a>
+         <li v-for="bg in backgrounds" v-bind:key="bg.Name">
+           <BackgroundCard :title="bg.Name" v-on:click="changeBg(bg)"></BackgroundCard>
          </li>
       </ul>
-      <h2>Ecosystem</h2>
-      <ul>
-         <li>
-            <a href="http://router.vuejs.org/" target="_blank">
-            vue-router
-            </a>
-         </li>
-         <li>
-            <a href="http://vuex.vuejs.org/" target="_blank">
-            vuex
-            </a>
-         </li>
-         <li>
-            <a href="http://vue-loader.vuejs.org/" target="_blank">
-            vue-loader
-            </a>
-         </li>
-         <li>
-            <a href="https://github.com/vuejs/awesome-vue" target="_blank">
-            awesome-vue
-            </a>
-         </li>
-      </ul>
-   </div>
+    </div>
+    <div>
+
+    </div>
+  </div>
 </template>
 
 <script>
+import BackgroundCard from './BackgroundCard.vue';
 export default {
   name: "Main",
-
+  components: {
+    BackgroundCard
+  },
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: "Welcome to Your Vue.js App",
+      backgrounds: [
+        {'Name':'City','Path':'../assets/field.jpg'},
+        {'Name':'Fields','Path':'../assets/city.jpg'}
+      ]
     };
+  },
+  methods: {
+    changeBg(bg){
+      console.log(bg.Name)
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+body{
+  background-color:rgb(236, 236, 236);
+}
+</style>
 <style scoped>
 .main {
   text-align: center;
+}
+
+.backgrounds {
+  background-color: rgb(156, 156, 156);
+  margin-left: auto;
+  margin-right: 50px;
+  margin-top: 0px;
+  min-width: 500px;
+  max-width: 30%;
+  border: solid;
+  border-width: 2px;
 }
 
 h1,
@@ -82,17 +68,13 @@ h2 {
 
 ul {
   list-style-type: none;
-
   padding: 0;
 }
 
 li {
-  display: inline-block;
-
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+  display: inline;
+  margin: auto;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 </style>
