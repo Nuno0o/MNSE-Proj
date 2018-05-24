b3e6fb1e9f94d94d3d9b0f3ae1cde082996a7e7c
tirafesi <tirafesi@hotmail.com>
2018-05-24 18:57:38 +0100

    added structure for multiple visual assets

--- proj/src/components/Main.vue
+++ proj/src/components/Main.vue
@@ -4,6 +4,9 @@
     <button id="toggle" @click="toggle">Toggle Interface</button>
     <!-- Background image -->
     <img id="background" v-bind:src="background_image">
+    <div v-for="asset in active_assets" v-bind:key="asset">
+      <img class="asset-image" v-bind:src="require('../assets/' + asset.Image)" />
+    </div>
     <!-- Background interface -->
     <div class="interface backgrounds">
       <h2>Background</h2>
@@ -13,11 +16,18 @@
          </li>
          <li></li>
       </ul>
+    </div>
+    <div class="interface secondary-backgrounds">
+        <h2>Assets</h2>
+        <ul>
+          <li v-for="asset in active_background.Assets" v-bind:key="asset">
+           <BackgroundCard :title="asset.Name" v-on:click="changeAsset(asset)"></BackgroundCard>
+         </li>
+        </ul>
+    </div>
+    <div class="interface volume">
       <h2>Volume</h2>
       <VolumeSlider id="volume-slider" @input="changeVolume"></VolumeSlider>
-    </div>
-    <div>
-
     </div>
   </div>
 </template>
@@ -33,33 +43,72 @@ export default {
   },
   data() {
     return {
+      visibility: 'hidden',
       show_interface: true,
       background_audio: null,
       background_volume: 0.5,
+      active_background: {},
       backgrounds: [
-        {'Name':'City','Image':'city.jpg','Sound':'street.mp3'},
+        {'Name':'City','Image':'city.jpg','Sound':'street.mp3', 'Assets': [{'Name': 'Rain', 'Image': 'rain.gif', 'Sound': 'rain.mp3'}]},
         {'Name':'Field','Image':'field.jpg','Sound':'field.mp3'},
         {'Name':'Beach','Image':'beach.jpg','Sound':'beach.mp3'},
         {'Name':'Birds','Image':'birds.jpg','Sound':'birds.mp3'},
-        {'Name':'Forest','Image':'forest.jpg','Sound':'rain.mp3'}
+        {'Name':'Forest','Image':'forest.jpg','Sound':'null'}
       ],
       background_image: require('../assets/placeholder.png'),
+      active_assets: [],
     };
+  },
+  computed: {
+    computedVisibility: function() {
+      return this.visibility;
+    }
   },
   methods: {
     changeVolume(volume) {
       this.background_volume = volume / 100.0
       this.background_audio.volume = this.background_volume
     },
+    changeAsset(asset) {
+      var found = this.active_assets.find(function(elem) {
+        return elem.Name == asset.Name
+      })
+
+      if (found == undefined)
+      {
+        this.active_assets.push(asset)
+      } else {
+        var index = this.active_assets.indexOf(found)
+        this.active_assets.splice(index, 1)
+      }
+
+      console.log(this.active_assets)
+
+      if(this.background_audio != null){
+        this.background_audio.pause()
+        this.background_audio = null
+      }
+      if (asset.Sound != 'null' && asset.Sound != undefined)
+      {
+        this.background_audio = new Audio(require('../assets/' + asset.Sound))
+        this.background_audio.volume = this.background_volume
+        this.background_audio.play()
+      }   
+    },
     changeBackground(bg){
+      this.active_assets = []
+      this.active_background = bg
       this.background_image = require('../assets/' + bg.Image)
       if(this.background_audio != null){
         this.background_audio.pause()
         this.background_audio = null
       }
-      this.background_audio = new Audio(require('../assets/' + bg.Sound))
-      this.background_audio.volume = this.background_volume
-      this.background_audio.play()
+      if (bg.Sound != 'null' && bg.Sound != undefined)
+      {
+        this.background_audio = new Audio(require('../assets/' + bg.Sound))
+        this.background_audio.volume = this.background_volume
+        this.background_audio.play()
+      }      
     },
     toggle () {
       if (this.show_interface) {
@@ -104,7 +153,7 @@ body{
   background-color:rgb(201, 201, 201);
 }
 
-#background {
+#background, .asset-image {
   left:0;
   top:0;
   right:0;
@@ -117,7 +166,7 @@ body{
   z-index: -1;
 }
 
-.backgrounds {
+.interface {
   background-color: rgb(156, 156, 156);
   margin-left: auto;
   margin-right: 10px;
